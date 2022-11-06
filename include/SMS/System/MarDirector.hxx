fefa77acea05af1733c1e9d7dd8cb37707f1c399
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2022-11-06 11:55:45 -0600

    Update old path

--- include/SMS/System/MarDirector.hxx
+++ include/SMS/System/MarDirector.hxx
@@ -15,7 +15,7 @@
 #include <SMS/MoveBG/Shine.hxx>
 
 #include <SMS/GC2D/GCConsole2.hxx>
-#include <SMS/npc/BaseNPC.hxx>
+#include <SMS/NPC/NpcBase.hxx>
 #include <SMS/MoveBG/DemoCannon.hxx>
 #include <SMS/GC2D/ShineFader.hxx>
 
