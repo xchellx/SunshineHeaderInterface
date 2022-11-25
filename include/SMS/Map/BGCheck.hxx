9cf21cf5a5882282f9138b7f46d4e49f3622be3e
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2022-11-24 21:02:31 -0600

    Update collision members

--- include/SMS/Map/BGCheck.hxx
+++ include/SMS/Map/BGCheck.hxx
@@ -18,18 +18,16 @@ public:
     bool isWaterSurface() const;
     void setVertex(const TVec3f &a, const TVec3f &b, const TVec3f &c);
 
-    u16 mCollisionType;  // 0x0000
-    s16 mValue4;         // 0x0002
-    u16 mIllegalFlags;   // 0x0004 | 0x10 = illegal
-    u8 mTerrainType;     // 0x0006 | Sound ID
-    f32 mMinHeight;      // 0x0008
-    f32 mMaxHeight;      // 0x000C
-    TVec3f mVertexA;     // 0x0010
-    TVec3f mVertexB;     // 0x001C
-    TVec3f mVertexC;     // 0x0028
-    TVec3f mNormal;      // 0x0034
-    f32 _40;
-    TLiveActor *mOwner;  // 0x0044
+    u16 mType;             // 0x0000
+    s16 mValue;            // 0x0002
+    u16 mFlags;            // 0x0004 | 0x10 = illegal
+    u8 mSoundID;           // 0x0006 | Sound ID
+    f32 mMinHeight;        // 0x0008
+    f32 mMaxHeight;        // 0x000C
+    TVec3f mVertices[3];   // 0x0010
+    TVec3f mNormal;        // 0x0034
+    f32 mProjectionFactor; // 0x0040
+    TLiveActor *mOwner;    // 0x0044
 };
 
 class TBGCheckList {
@@ -45,7 +43,9 @@ class TBGCheckListRoot {
 public:
     TBGCheckListRoot();
 
-    TBGCheckList mCheckLists[3];  // 0x0000
+    TBGCheckList mGround;  // 0x0000
+    TBGCheckList mRoof;  // 0x0004
+    TBGCheckList mWall;  // 0x0008
 };
 
 class TBGCheckListWarp : public TBGCheckList {
