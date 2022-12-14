459ec6f90a9d9545de22c5420c5d25cb15fc0369
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2022-12-14 17:40:40 -0600

    Add pol types

--- include/SMS/Map/PollutionLayer.hxx
+++ include/SMS/Map/PollutionLayer.hxx
@@ -4,6 +4,20 @@
 #include <SMS/Map/PollutionPos.hxx>
 #include <SMS/Map/Joint.hxx>
 
+enum PollutionType {
+    POLLUTION_SINK,
+    POLLUTION_FIRE,
+    POLLUTION_STOP,
+    POLLUTION_SLIP,
+    POLLUTION_ELEC,
+    POLLUTION_SINK_DIE,
+    POLLUTION_NULL1,
+    POLLUTION_BURN,
+    POLLUTION_NULL2,
+    POLLUTION_NULL3,
+    POLLUTION_NONE
+};
+
 class TPollutionLayer : public TJointModel {
 
 public:
