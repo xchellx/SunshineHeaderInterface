c98fe6a6abed5c9a7aa1a03cee9e9f3a85b661cb
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-01-03 19:05:48 -0600

    Finish TPair

--- include/JSystem/JGadget/Pair.hxx
+++ include/JSystem/JGadget/Pair.hxx
@@ -9,16 +9,50 @@ namespace JGadget {
         typedef T1 first_type;
         typedef T2 second_type;
 
-		constexpr TPair() = default;
+		_GLIBCXX_CONSTEXPR TPair() = default;
         TPair(const TPair &p) = default;
+
+#if __cplusplus >= 201103L
         TPair(TPair &&p) = default;
-        TPair(const first_type &a, const second_type &b)
+#endif
+
+        _GLIBCXX14_CONSTEXPR TPair(const first_type &a, const second_type &b)
             : first(a), second(b) {}
 
-        TPair &operator=(const TPair& p) {
+#if __cplusplus >= 201103L
+        template <class U1, class U2>
+        _GLIBCXX14_CONSTEXPR TPair(U1 &&a, U2 &&b) : first(a), second(b) {}
+#endif
+
+        template <class U1, class U2>
+        explicit _GLIBCXX14_CONSTEXPR TPair(const TPair<U1, U2> &pair)
+            : first(pair.first), second(pair.second) {}
+
+#if __cplusplus >= 201103L
+        template <class U1, class U2>
+        explicit _GLIBCXX14_CONSTEXPR TPair(TPair<U1, U2> &&pair)
+            : first(pair.first), second(pair.second) {}
+#endif
+
+        TPair &operator=(const TPair &p) {
             first = p.first;
             second = p.second;
         }
+
+#if __cplusplus >= 201103L
+        template <class U1, class U2>
+        TPair &operator=(const TPair<U1, U2> &p) {
+            first  = p.first;
+            second = p.second;
+        }
+
+        TPair &operator=(TPair &&p)
+            _GLIBCXX_NOEXCEPT_IF(JSystem::is_nothrow_move_assignable<T1>::value
+                                     &&JSystem::is_nothrow_move_assignable<T2>::value) {
+            first  = p.first;
+            second = p.second;
+        }
+#endif
 
 		first_type first;
         second_type second;
