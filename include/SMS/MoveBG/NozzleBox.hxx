74f126859f1370754c542aef22ccfac9ed494724
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-05-22 10:39:57 -0500

    Map out TNozzleBox

--- include/SMS/MoveBG/NozzleBox.hxx
+++ include/SMS/MoveBG/NozzleBox.hxx
@@ -1,21 +1,27 @@
 #pragma once
 
 #include <Dolphin/types.h>
+#include <Dolphin/GX.h>
 #include <SMS/MoveBG/Item.hxx>
 #include <SMS/MapObj/MapObjGeneral.hxx>
 
 class TNozzleBox : public TMapObjGeneral {
-
 public:
-    u32 _00[0x10 / 4];   // 0x0138
-    u32 mNozzleToRegen;  // 0x0148
+    TNozzleBox(const char *name);
+    ~TNozzleBox() override;
+
+    void load(JSUMemoryInputStream &) override;
+    void loadAfter() override;
+    bool receiveMessage(THitActor *, u32) override;
+
+    void makeModelValid();
+
+    u32 mNozzleType;  // 0x0148
     TItem *mNozzleItem;  // 0x014C
     f32 mThrowLength;    // 0x0150
     f32 mThrowHeight;    // 0x0154
-    char *mStringID;     // 0x0158
+    char *mBoxName;     // 0x0158
     bool mIsValid;       // 0x015C
-    u8 _01;              // 0x015D
-    u16 _02;             // 0x015E
-    u16 _03;             // 0x0160
-    u16 _04;             // 0x0162
+    GXColorS10 mMatColor; // 0x015E
+    bool mBalloonShown;
 };
