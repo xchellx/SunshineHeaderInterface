4960539959d17b9e87090a436920fe73222cf523
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-08-11 04:19:21 -0500

    Fix offset calculation

--- include/SMS/GC2D/GCConsole2.hxx
+++ include/SMS/GC2D/GCConsole2.hxx
@@ -55,7 +55,7 @@ public:
     J2DPicture *mWaterTopPanel;   // 0x02A0
     u32 _09[0x14 / 4];            // 0x02A4
     u32 mWaterCardTopHeight;      // 0x02B8
-    u32 _10[0x248 / 4];           // 0x02BC
+    u32 _10[0x170 / 4];           // 0x02BC
     TExPane *mRedCoinPanelBack;   // 0x042C
     TExPane *mRedCoinPanel;       // 0x0430
     TExPane *_434;                // 0x0434
