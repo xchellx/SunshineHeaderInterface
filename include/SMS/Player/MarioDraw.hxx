b3d3341730fd39f24678c1dae8885ccf306fd796
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2024-01-04 22:36:22 -0600

    Update member name and doc

--- include/SMS/Player/MarioDraw.hxx
+++ include/SMS/Player/MarioDraw.hxx
@@ -20,8 +20,7 @@ struct TMarioAnimeData {
     u8 mAnmTexPattern;  // Note: 23 is the max value allowed
     u8 mMarioHand;      // Note: 1, 2, and 3 seem to be valid values, this determines
                         // the hand model used(?)
-    u8 unk_1;           // Note: If bit 30 is set, it seems to activate a bit flag to
-                        // multiple pointers, likely mtx related
+    u8 mBodyFlags;      // Note: If bit 30 is set (0x2), enables jiggle physics
     u8 unk_2;           // Note: Value seems to be set but is never used?
 };
 
