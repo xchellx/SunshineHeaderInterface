0d067467bb7275f3d87559450b256825aaa28a68
JoshuaMKW <joshuamkw2002@gmail.com>
2024-06-05 09:46:48 -0500

    Lol

--- include/JSystem/J3D/J3DTransform.hxx
+++ include/JSystem/J3D/J3DTransform.hxx
@@ -4,15 +4,15 @@
 #include <Dolphin/types.h>
 
 struct J3DTransformInfo {
-    f32 tx;
-    f32 ty;
-    f32 tz;
-    s16 rx;
-    s16 ry;
-    s16 rz;
     f32 sx;
     f32 sy;
     f32 sz;
+    s16 rx;
+    s16 ry;
+    s16 rz;
+    f32 tx;
+    f32 ty;
+    f32 tz;
 };
 
 void J3DGetTranslateRotateMtx(s16 rx, s16 ry, s16 rz, f32 tx, f32 ty, f32 tz, Mtx out);
